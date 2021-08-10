ccd0b19a8e9d293ad1543e184b2ca98d9d726aef
igorrloliveira <igorrech@gmail.com>
2021-08-10 18:50:27 -0300

    first commit

---
+++ Developer/monitoramento-web-master/pages/index.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <h1>Monitoramento PJE</h1>
+      <v-data-table
+        :headers="headers"
+        :items="lista"
+        :items-per-page="10"
+        class="elevation-1"
+      ></v-data-table>
+
+  </div>
+</template>
+
+<script>
+
+export default {
+
+  data () {
+    return {
+      lista: [],
+      headers: [
+          { text: 'Titulo', value: 'dsTitulo' },
+        ],
+      }
+  },
+  mounted () {
+    this.carregar()
+  },
+  methods: {
+    carregar (params) {
+      this.$axios.get('https://pjeportal-desenv.apps.tjdft.jus.br/monitoramento/consulta').then(response => {
+        this.lista = response.data.results
+      })
+    },
+  }
+}
+</script>
+
+<style>
+.h1 {
+  color: rgb(255, 255, 255);
+}
+
+.table {
+  padding: 0px;
+  border: 1px #ffffff;
+  display: block;
+  margin: 20px;
+}
+</style>
